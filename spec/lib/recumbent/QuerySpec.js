c10f07a71cd780ce671c82d5039b5eaca368b094
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-10-14 14:27:10 -0400

    Can create  query by view, given a design document and view name.

--- spec/lib/recumbent/QuerySpec.js
+++ spec/lib/recumbent/QuerySpec.js
@@ -5,34 +5,27 @@ var Writer = require('../../../lib/recumbent/Writer');
 
 describe('Query', function () {
 
-  var database, server;
-
-  beforeEach(function (done) {
-    server = new Server({ url: 'http://localhost:5984', database: 'query_testing' });
-    database = new Database({ server: server });
-    done();
-  });
-
-  afterEach(function (done) {
-    database.destroy(function (error, result) {
-      done();
-    });
-  });
+  var database, docCount = 0, doneFunc, options, writeCount = 0;
 
   describe('Get document by ID', function () {
 
     beforeEach(function (done) {
-      database.destroy(function (error, result) {
-        database.create(function (error, result) {
-          (new Writer({ server: server })).data({ _id: 'my-test', field: 'value' }).exec(function (error, result) {
-            done(error);
-          });
-        });
+      docCount = 1;
+      writeCount = 0;
+      doneFunc = done;
+      createDatabase(function (error, result) {
+        insertDocument({ _id: 'my-test', field: 'value' }, onWrite);
+      });
+    });
+
+    afterEach(function (done) {
+      destroyDatabase(function (error, result) {
+        done();
       });
     });
 
     it('can fetch a document by id', function (done) {
-      var query = new Query({ server: server });
+      var query = new Query(options);
       query.id('my-test').exec(function (error, result) {
         if (error) {
           done(error);
@@ -48,75 +41,86 @@ describe('Query', function () {
 
   describe('Get documents by view', function () {
 
-    var docCount = 0, doneFunc;
-
     beforeEach(function (done) {
       doneFunc = done;
-      database.destroy(function (error, result) {
-        database.create(function (error, result) {
-          for (var i = 0; i < 10; i += 1) {
-            var doc = {
-              _id: i.toString(),
-              value: i
-            };
-            (new Writer({ server: server })).data(doc).exec(onWrite);
-          }
-          var view = {
-            _id: '_design/all_docs',
-            views: {
-              all_ids: {
-                map: function (doc) {
-                  emit(doc._id, null);
-                }.toString()
-              },
-              all_values: {
-                map: function (doc) {
-                  emit(doc.value, null);
-                }.toString()
-              }
-            }
-          };
-          (new Writer({ server: server })).data(view).exec(onWrite);
-        });
+      docCount = 11;
+      writeCount = 0;
+      createDatabase(function (error, result) {
+        for (var i = 0; i < 10; i += 1) {
+          insertDocument({ value: i }, onWrite);
+        }
+        insertDocument(getView(), onWrite);
       });
     });
 
-    it('can get a view by id', function (done) {
-      done();
-      // query.id('_design/all_docs/_view/all_ids').exec(function (error, result) {
-      //   if (error) {
-      //     done(error);
-      //   }
-      //   expect(result.total_rows).toEqual(10);
-      //   done();
-      // });
+    afterEach(function (done) {
+      destroyDatabase(function (error, result) {
+        done();
+      });
     });
 
-    xit('can get documents by view', function (done) {
-      done();
-      // (new Query({ server: server }))
-      //     .designDocument('all_docs')
-      //     .view('all_values')
-      //     .exec(function (error, result) {
-      //   if (error) {
-      //     console.error(error);
-      //     done(error);
-      //   }
-      //   expect(result.total_rows).toEqual(10);
-      //   done();
-      // });
+    it('can get documents by view', function (done) {
+      var query = new Query(options);
+      query.designDocument('all_docs').view('all_values').exec(function (error, result) {
+        if (error) {
+          console.error(error);
+          done(error);
+        }
+        expect(result.total_rows).toEqual(10);
+        expect(result.rows.length).toEqual(10);
+        done();
+      });
     });
-
-    function onWrite(error, result) {
-      if (error) {
-        doneFunc(error);
-      }
-      docCount += 1;
-      if (docCount === 11) {
-        doneFunc();
-      }
-    }
 
   });
+
+  function createDatabase(callback) {
+    setTimeout(function () {
+      options = { url: 'http://localhost:5984', database: 'query_testing' };
+      database = new Database(options);
+      database.create(callback);
+    }, 1);
+  }
+
+  function destroyDatabase(callback) {
+    setTimeout(function () {
+      database.destroy(callback);
+    }, 1);
+  }
+
+  function getView() {
+    return {
+      _id: '_design/all_docs',
+      views: {
+        all_ids: {
+          map: function (doc) {
+            emit(doc._id, null);
+          }.toString()
+        },
+        all_values: {
+          map: function (doc) {
+            emit(doc.value, null);
+          }.toString()
+        }
+      }
+    };
+  }
+
+  function insertDocument(doc, callback) {
+    setTimeout(function () {
+      var writer = new Writer(options);
+      writer.data(doc).exec(callback);
+    }, 1);
+  }
+
+  function onWrite(error, result) {
+    if (error) {
+      doneFunc(error);
+    }
+    writeCount += 1;
+    if (writeCount === docCount) {
+      doneFunc();
+    }
+  }
 
 });
