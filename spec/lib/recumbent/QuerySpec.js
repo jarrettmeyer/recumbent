e2503e9c7736abccd3b79f186a4fcaa632f4e568
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-10-15 10:17:37 -0400

    Adds support for querying a view with skip and limit.

--- spec/lib/recumbent/QuerySpec.js
+++ spec/lib/recumbent/QuerySpec.js
@@ -75,6 +75,43 @@ describe('Query', function () {
 
   });
 
+  describe('Get documents by skip and limit', function () {
+
+    beforeEach(function (done) {
+      doneFunc = done;
+      docCount = 11;
+      writeCount = 0;
+      createDatabase(function (error, result) {
+        for (var i = 0; i < 10; i += 1) {
+          insertDocument({ value: i }, onWrite);
+        }
+        insertDocument(getView(), onWrite);
+      });
+    });
+
+    afterEach(function (done) {
+      destroyDatabase(function (error, result) {
+        done();
+      });
+    });
+
+    it('can get documents from a view with skip and limit', function (done) {
+      var query = new Query(options);
+      query.designDocument('all_docs').view('all_values').skip(2).limit(2).exec(function (error, result) {
+        if (error) {
+          console.error(error);
+          done(error);
+        }
+        expect(result.total_rows).toEqual(10);
+        expect(result.rows.length).toEqual(2);
+        expect(result.rows[0].key).toEqual(2);
+        expect(result.rows[1].key).toEqual(3);
+        done();
+      });
+    });
+
+  });
+
   describe('Get documents by startkey and endkey', function () {
 
     beforeEach(function (done) {
